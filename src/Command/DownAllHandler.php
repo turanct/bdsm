013fe57cb1c397abc07ac1a6b5824ecc142c9115
turanct <spinnewebber_toon@hotmail.com>
2015-09-03 13:21:57 +0200

    Add downAll command & handler

---
+++ src/Command/DownAllHandler.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Bdsm\Command;
+
+use Bdsm\Locater;
+use Bdsm\Log;
+use Bdsm\Database;
+
+final class DownAllHandler
+{
+    private $locater;
+    private $log;
+    private $database;
+
+    public function __construct(
+        Locater $locater,
+        Log $log,
+        Database $database
+    ) {
+        $this->locater = $locater;
+        $this->log = $log;
+        $this->database = $database;
+    }
+
+    public function handle(DownAll $command)
+    {
+        $migrations = $this->locater->findMigrations();
+
+        $log = $this->log->get();
+
+        foreach ($migrations as $migration) {
+            $migrationId = get_class($migration);
+
+            if (
+                array_key_exists($migrationId, $log) === false
+                || $log[$migrationId] === 'skipped'
+            ) {
+                continue;
+            }
+
+            $migration->down($this->database);
+        }
+    }
+}
---
+++ tests/Command/DownAllHandlerTest.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Bdsm\Command;
+
+use Bdsm\Migration;
+use Bdsm\Database;
+use Exception;
+
+class DownAllHandlerTest extends \PHPUnit_Framework_TestCase
+{
+    function test_it_runs_multiple_migrations()
+    {
+        $locater = $this->getMockBuilder('\\Bdsm\\Locater')->getMock();
+        $locater->method('findMigrations')->willReturn(array(
+            new FirstDownMigration,
+            new SecondDownMigration,
+        ));
+
+        $log = $this->getMockBuilder('\\Bdsm\\Log')->getMock();
+        $log->method('get')->willReturn(array(
+            'Bdsm\\Command\\FirstDownMigration' => 'done',
+            'Bdsm\\Command\\SecondDownMigration' => 'done',
+        ));
+
+        $database = $this->getMockBuilder('\\Bdsm\\Database')->getMock();
+        $database
+            ->expects($this->exactly(2))
+            ->method('query');
+
+        $upone = new DownAll('Foo');
+        $handler = new DownAllHandler($locater, $log, $database);
+        $handler->handle($upone);
+    }
+
+    function test_it_skips_not_done_and_skipped_migrations()
+    {
+        $locater = $this->getMockBuilder('\\Bdsm\\Locater')->getMock();
+        $locater->method('findMigrations')->willReturn(array(
+            new NotDoneDownMigration,
+            new SkippedDownMigration,
+            new FirstDownMigration,
+        ));
+
+        $log = $this->getMockBuilder('\\Bdsm\\Log')->getMock();
+        $log->method('get')->willReturn(array(
+            'Bdsm\\Command\\FirstDownMigration' => 'done',
+            'Bdsm\\Command\\SkippedDownMigration' => 'skipped',
+        ));
+
+        $database = $this->getMockBuilder('\\Bdsm\\Database')->getMock();
+        $database
+            ->expects($this->once())
+            ->method('query');
+
+        $upone = new DownAll('Foo');
+        $handler = new DownAllHandler($locater, $log, $database);
+        $handler->handle($upone);
+    }
+}
+
+class NotDoneDownMigration implements Migration
+{
+    public function up(Database $database)
+    {
+        throw new Exception('failed');
+    }
+
+    public function down(Database $database)
+    {
+        throw new Exception('failed');
+    }
+}
+
+final class SkippedDownMigration extends NotDoneDownMigration {}
+
+class FirstDownMigration implements Migration
+{
+    public function up(Database $database)
+    {
+        throw new Exception('failed');
+    }
+
+    public function down(Database $database)
+    {
+        $database->query('foo');
+    }
+}
+
+final class SecondDownMigration extends FirstDownMigration {}
